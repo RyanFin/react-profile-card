d3dcec623e8af933f025a6785bca9ba2b95a746b
RyanFin <ryanfinlayson125@hotmail.com>
2024-03-19 01:38:27 +0000

    add profile card code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Challenge (Profile Card v1!)</h1>
+      <Card />
     </div>
   );
 }
 
-export default App;
+function Card() {
+  return (
+    <div className="card">
+      <Avatar src="ryan_suit.jpg" alt="Ryan Suit" />
+      <Intro />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <body className="data">
+      <h2 style={{ paddingBottom: "12px" }}>Ryan Finlayson</h2>
+      <p style={{ paddingTop: "0px" }}>
+        I am a Software Engineer and I am learning how to code in React JS. I
+        currently have a degree in Software Engineering and experience with
+        back-end development with Go.
+      </p>
+      <SkillList />
+    </body>
+  );
+}
+
+function Avatar(props) {
+  return <img src={props.src} alt={props.alt} className="avatar" />;
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skillName="Go" emoji="👌" color="lightblue" />
+      <Skill skillName="React JS" emoji="🤫" color="#3283ed" />
+      <Skill skillName="Python" emoji="🐍" color="green" />
+      <Skill skillName="JavaScript" emoji="🗣" color="yellow" />
+      <Skill skillName="Ruby-on-Rails" emoji="🌬" color="red" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div>
+      <h3 className="skill" style={{ backgroundColor: props.color }}>
+        <span>
+          {props.skillName} {props.emoji}
+        </span>
+      </h3>
+    </div>
+  );
+}
